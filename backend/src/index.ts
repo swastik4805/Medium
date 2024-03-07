502fd1c2dfa6fc40d7f2ddfb2956c7729098bcb8
SWASTIK CHATTERJEE <chatterjeeswastik7772@gmail.com>
2024-03-07 22:20:03 +0530

    major additions

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -30,3 +30,4 @@ app.get('/api/v1/blog/bulk',(c)=>{
 
 
 export default app
+//DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiZWNjNDdlMjctZjQwNS00YjBiLWEwNGEtNzNjZTdlYjA0OWIyIiwidGVuYW50X2lkIjoiNWMxZDkyNTA4ZTE0OTVlMzc4MTgyMmYzOTZmY2Y2Y2Q2OGQ3NjI5Yzc0MzYzMzY5OThhN2QwMjRkNDA5NDhkMSIsImludGVybmFsX3NlY3JldCI6ImM5ODFjZDBkLWI4NzgtNGM4NC05YzU3LWQ5NjY5ZGU4NTk3OCJ9.qtg1lEZmedrGOSn_DyFWPwWkKs_VQ5pDfpOFTnrI-lY"
